a10f38256ba581c29864bb8e63a3d2ea7f47787b
Egor Orlov <egororloff@gmail.com>
2024-03-24 23:58:45 +0300

    Refactor

--- app.js
+++ app.js
@@ -13,28 +13,37 @@ const profileMode = document.getElementById('profileMode');
 const profileBestOfBtn = document.getElementById('profileBestOfBtn');
 const profileBestOf = document.getElementById('profileBestOf');
 
-burgerMenu.addEventListener('click', function () {
-  this.classList.toggle('close');
-  overlay.classList.toggle('overlay');
-});
+if (burgerMenu) {
+  burgerMenu.addEventListener('click', function () {
+    this.classList.toggle('close');
+    overlay.classList.toggle('overlay');
+  });
+}
 
-burgerSideMenu.addEventListener('click', function () {
-  this.classList.toggle('close');
-  sideMenu.classList.toggle('overlay');
-});
+if (burgerSideMenu) {
+  burgerSideMenu.addEventListener('click', function () {
+    this.classList.toggle('close');
+    sideMenu.classList.toggle('overlay');
+  });
+}
 
+if (profileModeBtn) {
+  profileModeBtn.addEventListener('click', function () {
+    this.classList.toggle('close');
+    profileMode.classList.toggle('overlay');
+  });
+}
 
-profileModeBtn.addEventListener('click', function () {
-  this.classList.toggle('close');
-  profileMode.classList.toggle('overlay');
-});
+if (profileBestOfBtn) {
+  profileBestOfBtn.addEventListener('click', function () {
+    this.classList.toggle('close');
+    profileBestOf.classList.toggle('overlay');
+  });
+}
 
-profileBestOfBtn.addEventListener('click', function () {
-  this.classList.toggle('close');
-  profileBestOf.classList.toggle('overlay');
-});
-
-menuBtn.addEventListener('click', function () {
-  this.classList.toggle('close');
-  modeMenu.classList.toggle('overlay');
-});
+if (menuBtn) {
+  menuBtn.addEventListener('click', function () {
+    this.classList.toggle('close');
+    modeMenu.classList.toggle('overlay');
+  });
+}
